25f469896f1c5cdef10d2cae1fe2fe4b3bd01735
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-09-02 17:53:12 -0400

    ädds test and functionality to wallet

--- spec/walletSpec.js
+++ spec/walletSpec.js
@@ -15,14 +15,19 @@ describe("Wallet", function() {
 
   });
 
-  it("Should take money from Balance", function() {
+  it("Should take money from Balance - Exact Ammount to withdraw", function() {
       wallet_test = new wallet();
       expectedBalance = 0;
       expect(wallet_test.getBalance()).toBe(expectedBalance);
       
-      expectedBalance = 100;
-      wallet_test.takeBalance(expectedBalance);
-      expect(wallet_test.getBalance()).toBe(expectedBalance);
+      expectedBalance1 = 100;
+      wallet_test.addBalance(expectedBalance1);
+      expect(wallet_test.getBalance()).toBe(expectedBalance1);
+
+      expectedBalance2 = 0;
+      takeBalance = 100;
+      wallet_test.takeBalance(takeBalance);
+      expect(wallet_test.getBalance()).toBe(expectedBalance2);
   });
 
   it("Should add money to Balance", function() {
@@ -30,12 +35,28 @@ describe("Wallet", function() {
       expectedBalance = 0;
       expect(wallet_test.getBalance()).toBe(expectedBalance);
       
-      expectedBalance = 100;
-      wallet_test.addBalance(expectedBalance);
-      expect(wallet_test.getBalance()).toBe(expectedBalance);
+      expectedBalance1 = 100;
+      wallet_test.addBalance(expectedBalance1);
+      expect(wallet_test.getBalance()).toBe(expectedBalance1);
+
+      expectedBalance2 = 500;
+      wallet_test.addBalance(expectedBalance2);
+      expect(wallet_test.getBalance()).toBe(expectedBalance2 + expectedBalance1);
   });
 
   it("Should take money from Balance when 0 in place", function() {
+      wallet_test = new wallet();
+      expectedBalance = 0;
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
+
+      expectedBalance2 = 0;
+      takeBalance = 100;
+      wallet_test.takeBalance(takeBalance);
+      expect(wallet_test.getBalance()).toBe(expectedBalance2);
+
+  });
+
+  it("Should take money from Balance when not enough ", function() {
         pending("Not Yet Implemented")
 
   });
