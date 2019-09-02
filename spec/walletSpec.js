87ee9932dfd8b7b9926227c3937e42262a535855
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-09-02 09:52:05 -0400

    Initiatest Project

---
+++ spec/walletSpec.js
@@ -0,0 +1,23 @@
+describe("Wallet", function() {
+	
+  it("Should Check Class Wallet", function() {
+    
+  });
+
+  it("Should Check Wallet Balance", function() {
+    
+  });
+
+  it("Should take money from Balance", function() {
+    
+  });
+
+  it("Should add money to Balance", function() {
+    
+  });
+
+  it("Should take money from Balance when 0 in place", function() {
+    
+  });
+
+});
