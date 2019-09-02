a949d7a2a1abb939176a55b8706adf887bf18477
Alcides Ticlla <bandycoot_17@hotmail.com>
2019-09-02 17:23:35 -0400

    adds Wallet functionality

--- spec/walletSpec.js
+++ spec/walletSpec.js
@@ -1,23 +1,43 @@
+var Wallet = import('../src/wallet.js');
+
+
 describe("Wallet", function() {
-	
+
   it("Should Check Class Wallet", function() {
-    
+      wallet_test = new wallet();
+      expect(true).toBe(wallet_test instanceof wallet);
   });
 
-  it("Should Check Wallet Balance", function() {
-    
+  it("Should Check Wallet Balance property", function() {
+      wallet_test = new wallet();
+      expectedBalance = 0;
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
+
   });
 
   it("Should take money from Balance", function() {
-    
+      wallet_test = new wallet();
+      expectedBalance = 0;
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
+      
+      expectedBalance = 100;
+      wallet_test.takeBalance(expectedBalance);
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
   });
 
   it("Should add money to Balance", function() {
-    
+      wallet_test = new wallet();
+      expectedBalance = 0;
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
+      
+      expectedBalance = 100;
+      wallet_test.addBalance(expectedBalance);
+      expect(wallet_test.getBalance()).toBe(expectedBalance);
   });
 
   it("Should take money from Balance when 0 in place", function() {
-    
+        pending("Not Yet Implemented")
+
   });
 
 });
